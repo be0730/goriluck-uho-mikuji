a581ba137dcee5c98e9eea46d14e4684d0d9527f
be0730 <wkkey0730@gmail.com>
2025-07-17 14:25:53 +0900

    OPG画像出力修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'Your Site Name',
+      title: 'Default Title',
+      description: 'Default Description',
+      og: {
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        site_name: 'Your Site Name'
+      }
+    }
+  end
 end
