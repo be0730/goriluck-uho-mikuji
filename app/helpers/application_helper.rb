fb3a2f0ff50b07f2459c5b35da91bd9be548de71
be0730 <wkkey0730@gmail.com>
2025-07-18 21:52:38 +0900

    lintチェック

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,21 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: 'Your Site Name',
-      title: 'Default Title',
-      description: 'Default Description',
+      site: "Your Site Name",
+      title: "Default Title",
+      description: "Default Description",
       og: {
         title: :title,
         description: :description,
-        type: 'website',
+        type: "website",
         url: request.original_url,
-        site_name: 'Your Site Name'
+        site_name: "Your Site Name"
       }
     }
   end
 
   def auto_linebreak(text, width = 25)
-    return '' if text.blank?
+    return "" if text.blank?
     text.scan(/.{1,#{width}}/).join("\n")
   end
 end
