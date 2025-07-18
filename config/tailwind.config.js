0b191372e879732c86e61cf656ac58195f1d4f1c
be0730 <wkkey0730@gmail.com>
2025-07-18 22:43:45 +0900

    css修正

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,11 +1,15 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
+  ],
+  safelist: [
+    'text-responsive',
+    'responsive-text',
   ],
   theme: {
     extend: {
@@ -16,11 +20,12 @@ module.exports = {
         'vertical-open': {
           '0%':   { transform: 'scaleY(0)', 'border-radius': '3rem' },
           '70%':  { 'border-radius': '1.5rem' },
-          '100%': { transform: 'scaleY(1)', 'border-radius': '1rem' }
+          '100%': { transform: 'scaleY(1)', 'border-radius': '1rem' },
         },
       },
       animation: {
-        'vertical-open': 'vertical-open 1s cubic-bezier(0.68,-0.55,0.27,1.55) both',
+        'vertical-open':
+          'vertical-open 1s cubic-bezier(0.68,-0.55,0.27,1.55) both',
       },
     },
   },
@@ -28,5 +33,5 @@ module.exports = {
     // require('@tailwindcss/forms'),
     // require('@tailwindcss/typography'),
     // require('@tailwindcss/container-queries'),
-  ]
-}
+  ],
+};
