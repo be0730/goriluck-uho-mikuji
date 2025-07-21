00cf66575c2bf7e6cfa45db48d559f039e0a554d
be0730 <wkkey0730@gmail.com>
2025-07-21 20:40:40 +0900

    動的OGP削除

--- app/controllers/omikuji_controller.rb
+++ app/controllers/omikuji_controller.rb
@@ -12,35 +12,5 @@ class OmikujiController < ApplicationController
     return render plain: "Not Found", status: 404 if id < 0 || id >= list.size
 
     @result = list[id]
-
-    # 結果に対応するOGP画像パス
-    og_image_map = {
-      "ゴリラ大大吉" => "ogp/daidaikichi.png",
-      "大ウホ吉"     => "ogp/daiuhokichi.png",
-      "ウホ吉"       => "ogp/uhokichi.png",
-      "中ウホ吉"     => "ogp/chuuuhokichi.png",
-      "小ウホ吉"     => "ogp/syouuhokichi.png",
-      "末ウホ吉"     => "ogp/uhosuekichi.png",
-      "ウホ凶"       => "ogp/uhokyou.png"
-    }
-
-    # 結果に応じた画像URL（public/ogp にある画像を直接指す）
-    og_image_file = og_image_map[@result[:result]] || "ogp/default.png"
-    @og_image = "#{request.base_url}/#{og_image_file}"
-
-    # 動的OGPタグを設定
-    set_meta_tags(
-      title: "Goriluck ウホみくじ - #{@result[:result]}",
-      description: ActionController::Base.helpers.strip_tags(@result[:message].to_s),
-      og: {
-        title: "Goriluck ウホみくじ - #{@result[:result]}",
-        description: ActionController::Base.helpers.strip_tags(@result[:message].to_s),
-        image: @og_image,
-        url: request.original_url
-      },
-      twitter: {
-        image: @og_image
-      }
-    )
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,19 +2,22 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: "Your Site Name",
-      title: "Default Title",
-      description: "Default Description",
+      title: "Goriluck ウホみくじ",
+      description: "今日のあなたのウホ運勢をチェックしよう！",
       og: {
-        title: :title,
-        description: :description,
+        title: "Goriluck ウホみくじ",
+        description: "今日のあなたのウホ運勢をチェックしよう！",
         type: "website",
         url: request.original_url,
         site_name: "Your Site Name",
+        image: image_url("omikuji.png") # 配置するパスやファイル名によって変更
       },
       twitter: {
-        card: 'summary_large_image', # Twitterで表示する場合は大きいカードに変更
-        site: '@', # アプリの公式Twitterアカウントがあれば、アカウント名を記載
-        image: image_url('omikuji.png'),# 配置するパスやファイル名によって変更
+        title: "Goriluck ウホみくじ",
+        description: "今日のあなたのウホ運勢をチェックしよう！",
+        card: "summary_large_image", # Twitterで表示する場合は大きいカードに変更
+        site: "@", # アプリの公式Twitterアカウントがあれば、アカウント名を記載
+        image: image_url("omikuji.png") # 配置するパスやファイル名によって変更
       }
     }
   end
