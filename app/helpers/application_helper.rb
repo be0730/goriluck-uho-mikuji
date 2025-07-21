61a67dcbe55ac6bc81f2a5c103c6e6585b70fc3a
be0730 <wkkey0730@gmail.com>
2025-07-21 22:59:51 +0900

    修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,11 +10,9 @@ module ApplicationHelper
         type: "website",
         url: request.original_url,
         site_name: "Goriluck ウホみくじ",
-        image: "#{request.base_url}/ogp/omikuji.png" # 配置するパスやファイル名によって変更
+        image: image_url("/ogp/omikuji.png") # 配置するパスやファイル名によって変更
       },
       twitter: {
-        title: "Goriluck ウホみくじ",
-        description: "今日のあなたのウホ運勢をチェックしよう！",
         card: "summary_large_image", # Twitterで表示する場合は大きいカードに変更
         site: "@", # アプリの公式Twitterアカウントがあれば、アカウント名を記載
         image: image_url("/ogp/omikuji.png") # 配置するパスやファイル名によって変更
