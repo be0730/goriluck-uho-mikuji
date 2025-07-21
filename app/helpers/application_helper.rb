944a17245cd5ba24f29fa9fa289f3cc56240b353
be0730 <wkkey0730@gmail.com>
2025-07-21 15:35:34 +0900

    OGP修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,12 @@ module ApplicationHelper
         description: :description,
         type: "website",
         url: request.original_url,
-        site_name: "Your Site Name"
+        site_name: "Your Site Name",
+      },
+      twitter: {
+        card: 'summary_large_image', # Twitterで表示する場合は大きいカードに変更
+        site: '@', # アプリの公式Twitterアカウントがあれば、アカウント名を記載
+        image: image_url('omikuji.png'),# 配置するパスやファイル名によって変更
       }
     }
   end
